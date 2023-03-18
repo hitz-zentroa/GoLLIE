4d78949d6939de37ec7da31f89a19ce4a247aaf2
Oscar <oscar.sainz@ehu.eus>
2023-03-18 14:51:30 +0100

    Defining the directory structure

---
+++ src/tasks/ace/prompts.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+    """This class is used to instantiate persons, human beings."""
+    span: str
+
+
+ENTITY_DEFINITIONS = [
+    Person
+]
