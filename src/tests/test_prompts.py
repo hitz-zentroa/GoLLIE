aad753cb845202d9946506ab574dbcb780d2c424
Oscar <oscar.sainz@ehu.eus>
2023-03-21 10:17:20 +0100

    Add black formatting

--- src/tests/test_prompts.py
+++ src/tests/test_prompts.py
@@ -1,11 +1,10 @@
 import unittest
 import inspect
 
-class TestEntityPrompts(unittest.TestCase):
 
+class TestEntityPrompts(unittest.TestCase):
     def test_ACE(self):
         from src.tasks.ace.prompts import Person
+
         print()
         print(inspect.getsource(Person))
-
-
