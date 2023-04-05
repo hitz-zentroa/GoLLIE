32e2753c5443a6955869e4f0e98754b04ea5781e
OSainz <osainz59@gmail.com>
2023-04-05 13:37:34 +0200

    Fix: rams not in tasks init.

--- bash_scripts/generate_data.sh
+++ bash_scripts/generate_data.sh
@@ -6,5 +6,6 @@ OUTPUT_DIR="data/processed"
 python -m src.generate_data \
     --configs \
         ${CONFIG_DIR}/ace_config.json \
+        ${CONFIG_DIR}/rams_config.json \
     --output ${OUTPUT_DIR} \
     --overwrite_output_dir
