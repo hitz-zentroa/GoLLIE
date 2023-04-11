be409c788ace07c2a904fd00c09c9dea7d71fbea
Iker <igarciaf896@gmail.com>
2023-04-11 12:06:35 +0200

    hparams find

---
+++ bash_scripts/run_CoLLIE7B_hparam.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=CoLLIE7B
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128G
+#SBATCH --output=.slurm/CoLLIE7B.out.txt
+#SBATCH --error=.slurm/CoLLIE7B.err.txt
+
+source /gscratch4/users/osainz006/CoLLIE/venv/collie/bin/activate
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export TOKENIZERS_PARALLELISM=true
+export TRANSFORMERS_NO_ADVISORY_WARNINGS=true
+export WANDB_ENTITY=hitz-collie
+export WANDB_PROJECT=CoLLIE
+
+
+CONFIGS_FOLDER="configs/model_configs/hparams"
+
+# cd ../src || exit
+
+# Call this script from root directory as: sbatch bash_scripts/run_CoLLIE7B.sh
+
+# COSINE SCHEDULER
+python3 src/trainer.py ${CONFIGS_FOLDER}/CoLLIE-7B_optim_AdamW_lr_5e5_cosine.yaml
+python3 src/trainer.py ${CONFIGS_FOLDER}/CoLLIE-7B_optim_AdamW_lr_3e4_cosine.yaml
+
+#CONSTANT SCHEDULER
+python3 src/trainer.py ${CONFIGS_FOLDER}/CoLLIE-7B_optim_AdamW_lr_5e5_constant.yaml
+python3 src/trainer.py ${CONFIGS_FOLDER}/CoLLIE-7B_optim_AdamW_lr_3e4_constant.yaml
+
+# OTHER OPTIMIZERS
+python3 src/trainer.py ${CONFIGS_FOLDER}/CoLLIE-7B_optim_FusedAdamW_lr_3e4_cosine.yaml
+python3 src/trainer.py ${CONFIGS_FOLDER}/CoLLIE-7B_optim_AdaFactor_lr_1e4_cosine.yaml
+
