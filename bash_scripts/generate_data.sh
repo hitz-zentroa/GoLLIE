dc03c0eaa025ea977f1e178a224250fb6c94e534
Iker <igarciaf896@gmail.com>
2023-04-22 11:33:08 +0200

    generate conll

--- bash_scripts/generate_data.sh
+++ bash_scripts/generate_data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 CONFIG_DIR="configs/data_configs"
-OUTPUT_DIR="data/processed_black"
+OUTPUT_DIR="data/processed"
 
 python -m src.generate_data \
     --configs \
-        ${CONFIG_DIR}/ace_config.json \
-        ${CONFIG_DIR}/rams_config.json \
+        #${CONFIG_DIR}/ace_config.json \
+        #${CONFIG_DIR}/rams_config.json \
+        ${CONFIG_DIR}/conll03_config.json \
     --output ${OUTPUT_DIR} \
     --overwrite_output_dir
