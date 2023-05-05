7fc533d3028cb22280805538908f6490b098aae9
OSainz <osainz59@gmail.com>
2023-05-05 12:32:39 +0200

    Scorer and config fixes

--- src/tasks/ace/scorer.py
+++ src/tasks/ace/scorer.py
@@ -1,6 +1,13 @@
 from typing import Dict, List, Type
 
-from src.tasks.ace.prompts import ENTITY_DEFINITIONS, EVENT_DEFINITIONS, RELATION_DEFINITIONS, VALUE_DEFINITIONS
+from src.tasks.ace.prompts import (
+    COARSE_EVENT_DEFINITIONS,
+    COARSE_RELATION_DEFINITIONS,
+    ENTITY_DEFINITIONS,
+    EVENT_DEFINITIONS,
+    RELATION_DEFINITIONS,
+    VALUE_DEFINITIONS,
+)
 from src.tasks.utils_scorer import EventScorer, RelationScorer, SpanScorer
 from src.tasks.utils_typing import Entity, Value
 
@@ -25,13 +32,25 @@ class ACEValueScorer(SpanScorer):
         return {"values": output["spans"]}
 
 
+class ACECoarseRelationScorer(RelationScorer):
+    """ACE Relation identification scorer."""
+
+    valid_types: List[Type] = COARSE_RELATION_DEFINITIONS
+
+
 class ACERelationScorer(RelationScorer):
-    """ACE Relation identification and classification scorer."""
+    """ACE Relation identification scorer."""
 
     valid_types: List[Type] = RELATION_DEFINITIONS
 
 
 class ACEEventScorer(EventScorer):
+    """ACE Event and argument classification scorer."""
+
+    valid_types: List[Type] = COARSE_EVENT_DEFINITIONS
+
+
+class ACEEventArgumentScorer(EventScorer):
     """ACE Event and argument classification scorer."""
 
     valid_types: List[Type] = EVENT_DEFINITIONS
