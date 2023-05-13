a89c70181f24cb871a37432ed3d7f2b45263c657
ikergarciaf <igarciaf896@gmail.com>
2023-05-13 17:01:57 +0200

    Improve output format and fix bug

--- src/tasks/conll03/guidelines.py
+++ src/tasks/conll03/guidelines.py
@@ -1,40 +1,27 @@
 GUIDELINES = {
-    "ner_person": {"en": ["first, middle and last names of people, animals and fictional characters aliases."]},
+    "ner_person": {
+        "en": [
+            "first, middle and last names of people, animals and fictional characters aliases.",
+            '"Aliases for first, middle, and last names of individuals, animals, and fictional characters."',
+        ]
+    },
     "ner_organization": {
         "en": [
-            "Companies (press agencies, studios, banks, stock\n"
-            "markets, manufacturers, cooperatives) subdivisions of companies (newsrooms)\n"
-            "brands political movements (political parties, terrorist organisations)\n"
-            "government bodies (ministries, councils, courts, political unions of countries\n"
-            "(e.g. the {\\it U.N.})) publications (magazines, newspapers, journals)\n"
-            "musical companies (bands, choirs, opera companies, orchestras\n"
-            "public organisations (schools, universities, charities other collections\n"
-            "of people (sports clubs, sports teams, associations, theaters companies,\n"
-            "religious orders, youth organisations.\n"
+            "Companies (press agencies, studios, banks, stock\nmarkets, manufacturers, cooperatives) subdivisions of companies (newsrooms)\nbrands political movements (political parties, terrorist organisations)\ngovernment bodies (ministries, councils, courts, political unions of countries\n(e.g. the {\\it U.N.})) publications (magazines, newspapers, journals)\nmusical companies (bands, choirs, opera companies, orchestras\npublic organisations (schools, universities, charities other collections\nof people (sports clubs, sports teams, associations, theaters companies,\nreligious orders, youth organisations.\n",
+            "There are various types of organizations, such as press agencies, studios, banks, stock markets, manufacturers, cooperatives, and government bodies, that have subdivisions or brands, and some of these organizations have political movements or publications. There are also musical companies, public organizations such as schools and universities, and other collections of people such as sports clubs, religious orders, and youth organizations.",
         ]
     },
     "ner_location": {
         "en": [
-            "Roads (streets, motorways) trajectories regions (villages, towns, cities, provinces,\n"
-            "countries, continents, dioceses, parishes) structures (bridges, ports, dams) natural locations\n"
-            "(mountains, mountain ranges, woods, rivers, wells, fields, valleys, gardens, nature reserves,\n"
-            "allotments, beaches, national parks) public places (squares, opera houses, museums, schools, markets,\n"
-            "airports, stations, swimming pools, hospitals, sports facilities, youth centers, parks, town halls,\n"
-            "theaters, cinemas, galleries, camping grounds, NASA launch pads, club houses, universities, libraries,\n"
-            "churches, medical centers, parking lots, playgrounds, cemeteries) commercial places (chemists, pubs,\n"
-            "restaurants, depots, hostels, hotels, industrial parks, nightclubs, music venues) assorted buildings\n"
-            "(houses, monasteries, creches, mills, army barracks, castles, retirement homes, towers, halls, rooms,\n"
-            "vicarages, courtyards) abstract ``places'' (e.g. {\\it the free world})\n"
+            "Roads (streets, motorways) trajectories regions (villages, towns, cities, provinces,\ncountries, continents, dioceses, parishes) structures (bridges, ports, dams) natural locations\n(mountains, mountain ranges, woods, rivers, wells, fields, valleys, gardens, nature reserves,\nallotments, beaches, national parks) public places (squares, opera houses, museums, schools, markets,\nairports, stations, swimming pools, hospitals, sports facilities, youth centers, parks, town halls,\ntheaters, cinemas, galleries, camping grounds, NASA launch pads, club houses, universities, libraries,\nchurches, medical centers, parking lots, playgrounds, cemeteries) commercial places (chemists, pubs,\nrestaurants, depots, hostels, hotels, industrial parks, nightclubs, music venues) assorted buildings\n(houses, monasteries, creches, mills, army barracks, castles, retirement homes, towers, halls, rooms,\nvicarages, courtyards) abstract ``places'' (e.g. {\\it the free world})\n",
+            'The term "places" encompasses a wide range of physical locations, including roads and motorways, regions such as villages, towns, and countries, structures like bridges and ports, natural locations like rivers and forests, public places like squares and parks, and commercial places like pubs and hotels. Additionally, there are assorted buildings like houses, monasteries, and factories, and abstract concepts like "the free world."',
         ]
     },
     "ner_miscellaneous": {
         "en": [
-            "Words of which one part is a location, organisation, miscellaneous, or person adjectives\n"
-            "and other words derived from a word which is location, organisation, miscellaneous, or person\n"
-            "religions political ideologies nationalities languages programs events (conferences, festivals,\n"
-            "sports competitions, forums, parties, concerts) wars sports related names (league tables, leagues,\n"
-            "cups titles (books, songs, films, stories, albums, musicals, TV programs) slogans eras in time types\n"
-            "(not brands) of objects (car types, planes, motorbikes)\n"
+            "Words of which one part is a location, organisation, miscellaneous, or person adjectives\nand other words derived from a word which is location, organisation, miscellaneous, or person\nreligions political ideologies nationalities languages programs events (conferences, festivals,\nsports competitions, forums, parties, concerts) wars sports related names (league tables, leagues,\ncups titles (books, songs, films, stories, albums, musicals, TV programs) slogans eras in time types\n(not brands) of objects (car types, planes, motorbikes)\n",
+            "This text describes a list of nouns that can be used to describe various things, including location, organizations, miscellaneous items, and people. These nouns can be used to modify other words, such as adjectives derived from words that describe location, organizations, miscellaneous items, or people. Additionally, the text mentions specific types of events, such as conferences, festivals, sports competitions, forums, parties, concerts, and wars. It also mentions specific types of names, such as those related to sports leagues, cups, titles, and eras in time.",
         ]
     },
 }
+
