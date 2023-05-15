ebd766bfc13fa0db160acc4ad3aa9bf74a196ce8
Iker <igarciaf896@gmail.com>
2023-05-15 12:16:04 +0200

    Run tasks multiCPU

--- src/generate_data.py
+++ src/generate_data.py
@@ -6,6 +6,9 @@ from itertools import cycle
 from typing import Type
 
 from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
+import multiprocessing as mp
+from functools import partial
+import copy
 
 
 def get_class(class_path: str) -> Type:
@@ -17,131 +20,150 @@ def get_class(class_path: str) -> Type:
     return mod
 
 
+def multicpu_generator(args, config):
+    dataloader_cls = get_class(config["dataloader_cls"])
+    sampler_cls = get_class(config["sampler_cls"])
+    seeds = config.get("seed", 0)
+    if isinstance(seeds, int):
+        seeds = [seeds]
+    label_noise = config.get("label_noise_prob", 0.0)
+    if isinstance(label_noise, float):
+        label_noise = cycle([label_noise])
+
+    if "train_file" in config:
+        dataloader = dataloader_cls(config["train_file"], **config)
+        for ie_task in config["tasks"]:
+            for seed, noise_prob in zip(seeds, label_noise):
+                config["seed"] = seed
+                config["label_noise_prob"] = noise_prob
+                sampler = sampler_cls(
+                    dataloader,
+                    task=ie_task,
+                    split="train",
+                    **config,
+                    **config["task_configuration"][ie_task],
+                )
+
+                output_name = f"{config['dataset_name'].lower()}.{ie_task.lower()}.train.{seed}.jsonl"
+
+                if os.path.exists(os.path.join(args.output_dir, output_name)) and not args.overwrite_output_dir:
+                    logging.warning(f"Skipping {output_name} because it already exists.")
+                    continue
+
+                with open(os.path.join(args.output_dir, output_name), "w") as _file, Progress(
+                    SpinnerColumn(),
+                    *Progress.get_default_columns(),
+                    TimeElapsedColumn(),
+                ) as progress:
+                    task = progress.add_task(
+                        f"[cyan]{config['dataset_name']}-{ie_task}-train-{seed}",
+                        total=len(dataloader),
+                    )
+                    ids = []
+                    for elem in sampler:
+                        _file.write(f"{json.dumps(elem, ensure_ascii=False)}\n")
+                        if ids != elem["ids"]:
+                            ids = elem["ids"]
+                            progress.update(task, advance=len(ids))
+
+                logging.info(f"Data saved to {os.path.abspath(os.path.join(args.output_dir, output_name))}")
+
+    if "dev_file" in config:
+        config["seed"] = 0
+        dataloader = dataloader_cls(config["dev_file"], **config)
+        for task in config["tasks"]:
+            sampler = sampler_cls(
+                dataloader,
+                task=task,
+                split="dev",
+                **config,
+                **config["task_configuration"][task],
+            )
+
+            output_name = f"{config['dataset_name'].lower()}.{task.lower()}.dev.jsonl"
+
+            if os.path.exists(os.path.join(args.output_dir, output_name)) and not args.overwrite_output_dir:
+                logging.warning(f"Skipping {output_name} because it already exists.")
+                continue
+
+            with open(os.path.join(args.output_dir, output_name), "w") as _file, Progress(
+                SpinnerColumn(),
+                *Progress.get_default_columns(),
+                TimeElapsedColumn(),
+            ) as progress:
+                task = progress.add_task(
+                    f"[cyan]{config['dataset_name']}-{task}-dev",
+                    total=len(dataloader),
+                )
+                ids = []
+                for elem in sampler:
+                    _file.write(f"{json.dumps(elem, ensure_ascii=False)}\n")
+                    if ids != elem["ids"]:
+                        ids = elem["ids"]
+                        progress.update(task, advance=len(ids))
+
+            logging.info(f"Data saved to {os.path.abspath(os.path.join(args.output_dir, output_name))}")
+
+    if "test_file" in config:
+        config["seed"] = 0
+        dataloader = dataloader_cls(config["test_file"], **config)
+        for task in config["tasks"]:
+            sampler = sampler_cls(
+                dataloader,
+                task=task,
+                split="test",
+                **config,
+                **config["task_configuration"][task],
+            )
+
+            output_name = f"{config['dataset_name'].lower()}.{task.lower()}.test.jsonl"
+
+            if os.path.exists(os.path.join(args.output_dir, output_name)) and not args.overwrite_output_dir:
+                logging.warning(f"Skipping {output_name} because it already exists.")
+                continue
+
+            with open(os.path.join(args.output_dir, output_name), "w") as _file, Progress(
+                SpinnerColumn(),
+                *Progress.get_default_columns(),
+                TimeElapsedColumn(),
+            ) as progress:
+                task = progress.add_task(
+                    f"[cyan]{config['dataset_name']}-{task}-test",
+                    total=len(dataloader),
+                )
+                ids = []
+                for elem in sampler:
+                    _file.write(f"{json.dumps(elem, ensure_ascii=False)}\n")
+                    if ids != elem["ids"]:
+                        ids = elem["ids"]
+                        progress.update(task, advance=len(ids))
+
+            logging.info(f"Data saved to {os.path.abspath(os.path.join(args.output_dir, output_name))}")
+
+
 def main(args):
     os.makedirs(args.output_dir, exist_ok=True)
 
-    for config_file in args.configs:
+    config_files = args.configs
+    # We generate a new config for each train split, so we also parallelize over each split
+    configs = []
+    splits = ["train_file", "dev_file", "test_file"]
+    for config_file in config_files:
         with open(config_file, "rt") as f:
             config = json.load(f)
+            for file in splits:
+                new_config = copy.deepcopy(config)
+                if file in new_config:
+                    for other_split in splits:
+                        if other_split != file:
+                            if other_split in new_config:
+                                new_config.pop(other_split)
+                    configs.append(new_config)
 
-        dataloader_cls = get_class(config["dataloader_cls"])
-        sampler_cls = get_class(config["sampler_cls"])
-        seeds = config.get("seed", 0)
-        if isinstance(seeds, int):
-            seeds = [seeds]
-        label_noise = config.get("label_noise_prob", 0.0)
-        if isinstance(label_noise, float):
-            label_noise = cycle([label_noise])
+    generator_fn = partial(multicpu_generator, args)
 
-        if "train_file" in config:
-            dataloader = dataloader_cls(config["train_file"], **config)
-            for ie_task in config["tasks"]:
-                for seed, noise_prob in zip(seeds, label_noise):
-                    config["seed"] = seed
-                    config["label_noise_prob"] = noise_prob
-                    sampler = sampler_cls(
-                        dataloader,
-                        task=ie_task,
-                        split="train",
-                        **config,
-                        **config["task_configuration"][ie_task],
-                    )
-
-                    output_name = f"{config['dataset_name'].lower()}.{ie_task.lower()}.train.{seed}.jsonl"
-
-                    if os.path.exists(os.path.join(args.output_dir, output_name)) and not args.overwrite_output_dir:
-                        logging.warning(f"Skipping {output_name} because it already exists.")
-                        continue
-
-                    with open(os.path.join(args.output_dir, output_name), "w") as _file, Progress(
-                        SpinnerColumn(),
-                        *Progress.get_default_columns(),
-                        TimeElapsedColumn(),
-                    ) as progress:
-                        task = progress.add_task(
-                            f"[cyan]{config['dataset_name']}-{ie_task}-train-{seed}",
-                            total=len(dataloader),
-                        )
-                        ids = []
-                        for elem in sampler:
-                            _file.write(f"{json.dumps(elem, ensure_ascii=False)}\n")
-                            if ids != elem["ids"]:
-                                ids = elem["ids"]
-                                progress.update(task, advance=len(ids))
-
-                    logging.info(f"Data saved to {os.path.abspath(os.path.join(args.output_dir, output_name))}")
-
-        if "dev_file" in config:
-            config["seed"] = 0
-            dataloader = dataloader_cls(config["dev_file"], **config)
-            for task in config["tasks"]:
-                sampler = sampler_cls(
-                    dataloader,
-                    task=task,
-                    split="dev",
-                    **config,
-                    **config["task_configuration"][task],
-                )
-
-                output_name = f"{config['dataset_name'].lower()}.{task.lower()}.dev.jsonl"
-
-                if os.path.exists(os.path.join(args.output_dir, output_name)) and not args.overwrite_output_dir:
-                    logging.warning(f"Skipping {output_name} because it already exists.")
-                    continue
-
-                with open(os.path.join(args.output_dir, output_name), "w") as _file, Progress(
-                    SpinnerColumn(),
-                    *Progress.get_default_columns(),
-                    TimeElapsedColumn(),
-                ) as progress:
-                    task = progress.add_task(
-                        f"[cyan]{config['dataset_name']}-{task}-dev",
-                        total=len(dataloader),
-                    )
-                    ids = []
-                    for elem in sampler:
-                        _file.write(f"{json.dumps(elem, ensure_ascii=False)}\n")
-                        if ids != elem["ids"]:
-                            ids = elem["ids"]
-                            progress.update(task, advance=len(ids))
-
-                logging.info(f"Data saved to {os.path.abspath(os.path.join(args.output_dir, output_name))}")
-
-        if "test_file" in config:
-            config["seed"] = 0
-            dataloader = dataloader_cls(config["test_file"], **config)
-            for task in config["tasks"]:
-                sampler = sampler_cls(
-                    dataloader,
-                    task=task,
-                    split="test",
-                    **config,
-                    **config["task_configuration"][task],
-                )
-
-                output_name = f"{config['dataset_name'].lower()}.{task.lower()}.test.jsonl"
-
-                if os.path.exists(os.path.join(args.output_dir, output_name)) and not args.overwrite_output_dir:
-                    logging.warning(f"Skipping {output_name} because it already exists.")
-                    continue
-
-                with open(os.path.join(args.output_dir, output_name), "w") as _file, Progress(
-                    SpinnerColumn(),
-                    *Progress.get_default_columns(),
-                    TimeElapsedColumn(),
-                ) as progress:
-                    task = progress.add_task(
-                        f"[cyan]{config['dataset_name']}-{task}-test",
-                        total=len(dataloader),
-                    )
-                    ids = []
-                    for elem in sampler:
-                        _file.write(f"{json.dumps(elem, ensure_ascii=False)}\n")
-                        if ids != elem["ids"]:
-                            ids = elem["ids"]
-                            progress.update(task, advance=len(ids))
-
-                logging.info(f"Data saved to {os.path.abspath(os.path.join(args.output_dir, output_name))}")
+    with mp.Pool(processes=min(os.cpu_count(), len(configs))) as pool:
+        pool.map(generator_fn, configs)
 
 
 if __name__ == "__main__":
