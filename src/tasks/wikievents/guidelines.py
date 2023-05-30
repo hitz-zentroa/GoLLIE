a664484a28e2f628157bf03fba38bb655da412a5
OSainz <osainz59@gmail.com>
2023-05-30 12:38:00 +0200

    wikievents ner guidelines

---
+++ src/tasks/wikievents/guidelines.py
@@ -0,0 +1,89 @@
+GUIDELINES = {
+    "wikievents_ner_abstract": {
+        "en": [
+            "Abstract, non-tangible artifacts such as software (e.g., programs, tool kits, apps, e-mail), measureable"
+            " intellectual property, contracts, etc. (nb: does not include laws, which are LAW type)"
+        ]
+    },
+    "wikievents_ner_bodypart": {
+        "en": ["An identifiable, living part of a human's or animal's body, such as a eye, ear, neck, leg, etc."]
+    },
+    "wikievents_ner_commercialproduct": {
+        "en": [
+            "A tangible product or article of trade for which someone pays or barters, or more generally, an artifact"
+            " or a thing."
+        ]
+    },
+    "wikievents_ner_facility": {
+        "en": [
+            "A functional, primarily man-made structure. Facilities are artifacts falling under the domains of"
+            " architecture and civil engineering, including more temporary human constructs, such as police lines and"
+            " checkpoints."
+        ]
+    },
+    "wikievents_ner_gpe": {
+        "en": [
+            "Geopolitical entities such as countries, provinces, states, cities, towns, etc. GPEs are composite"
+            " entities, consisting of a physical location, a government, and a population. All three of these elements"
+            " must be present for an entity to be tagged as a GPE. A GPE entity may be a single geopolitical entity or"
+            " a group."
+        ]
+    },
+    "wikievents_ner_information": {
+        "en": [
+            "An information object such as a field of study or topic of communication, including thoughts, opinions,"
+            " etc."
+        ]
+    },
+    "wikievents_ner_location": {
+        "en": ["Geographical entities such as geographical areas and landmasses, bodies of water."]
+    },
+    "wikievents_ner_medicalhealthissue": {
+        "en": [
+            "Any medical condition or health issue, to include everything from disease to broken bones to fever to"
+            " general ill health, medical errors, even natural causes"
+        ]
+    },
+    "wikievents_ner_money": {
+        "en": [
+            "A monetary payment. The extent of a Money mention includes modifying quantifiers, the amount, and the"
+            " currency unit, all of which can be optional."
+        ]
+    },
+    "wikievents_ner_organization": {
+        "en": [
+            "Corporations, agencies, and other groups of people defined by an established organizational structure. An"
+            " ORG entity may be a single organization or a group. A key feature of an ORG is that it can change"
+            " members without changing identity."
+        ]
+    },
+    "wikievents_ner_person": {
+        "en": ["Person entities are limited to humans. A Person entity may be a single person or a group."]
+    },
+    "wikievents_ner_side_of_conflict": {
+        "en": [
+            "The different sides of a conflict, such as  philosophical, cultural, ideological, religious, political,"
+            " guiding philosophical movement or group orientation.  This will encompass sides of the battle/conflict,"
+            " sports fans when salient, and other such affiliations, in addition to religions, political parties, and"
+            " other philosophies."
+        ]
+    },
+    "wikievents_ner_job_title": {"en": ["A person's title or job role."]},
+    "wikievents_ner_numeric": {
+        "en": [
+            "A numerical value or non-numerical value such as an informational property such as color or make or URL."
+        ]
+    },
+    "wikievents_ner_vehicle": {
+        "en": [
+            "A physical device primarily designed to move an object from one location to another, by (for example)"
+            " carrying, flying, pulling, or pushing the transported object. Vehicle entities may or may not have their"
+            " own power source."
+        ]
+    },
+    "wikievents_ner_weapon": {
+        "en": [
+            "A physical device that is primarily used as an instrument for physically harming or destroying entities."
+        ]
+    },
+}
