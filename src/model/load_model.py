c73dd8665e7f8acc61f070fd1b0f8c8e4b416cb8
ikergarcia1996 <i.garciaferrerosanpelayo@gmail.com>
2023-06-01 18:25:01 +0200

    Minor tweaks

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -33,6 +33,7 @@ def load_model_for_training(
     lora_dropout: Optional[float] = 0.05,
     torch_dtype: Optional[str] = None,
     force_auto_device_map: bool = False,
+    use_gradient_checkpointing:bool = False,
 ) -> Tuple[PreTrainedModel, PreTrainedTokenizerBase]:
     """
     Load any Decoder model for training.
@@ -72,6 +73,8 @@ def load_model_for_training(
             Whether to force the use of the auto device map. If set to True, the model will be split across
             GPUs and CPU to fit the model in memory. If set to False, a full copy of the model will be loaded
             into each GPU. Defaults to False.
+        use_gradient_checkpointing (`bool`, optiona):
+            Whether to use gradient checkpointing for training
 
     Raises:
         `ValueError`:
@@ -132,7 +135,8 @@ def load_model_for_training(
             bnb_4bit_quant_type="nf4",
             bnb_4bit_compute_dtype=torch.bfloat16,
         )
-        torch_dtype = torch.bfloat16
+        if quantization==4:
+            torch_dtype = torch.bfloat16
         logging.info(f"Bits and Bytes config: {json.dumps(bnb_config.to_dict(),indent=4,ensure_ascii=False)}")
     else:
         torch_dtype = torch_dtype if torch_dtype in ["auto", None] else getattr(torch, torch_dtype)
@@ -201,8 +205,8 @@ def load_model_for_training(
     if quantization is not None:
         from peft import prepare_model_for_kbit_training
 
-        model.gradient_checkpointing_enable()
-        model = prepare_model_for_kbit_training(model)
+        #model.gradient_checkpointing_enable()
+        model = prepare_model_for_kbit_training(model,use_gradient_checkpointing=use_gradient_checkpointing)
 
     if use_lora:
         from peft import LoraConfig, PeftModel, TaskType, get_peft_model
