f0da4f279040fd0539ca8d183008a4d15dc5c6d0
OSainz <osainz59@gmail.com>
2023-06-02 16:22:09 +0200

    WikiEvents sampler [TODO: test]

--- src/tasks/wikievents/data_loader.py
+++ src/tasks/wikievents/data_loader.py
@@ -1,7 +1,14 @@
 import inspect
 import json
+from typing import Tuple, Union
+from src.tasks.wikievents.guidelines import GUIDELINES
 
 from src.tasks.wikievents.prompts import (
+    COARSE_EVENT_DEFINITIONS,
+    COARSE_TO_FINE_EVENTS,
+    ENTITY_DEFINITIONS,
+    EVENT_DEFINITIONS,
+    FINE_TO_COARSE_EVENTS,
     GPE,
     Abstract,
     Acquit,
@@ -71,7 +78,7 @@ from src.tasks.wikievents.prompts import (
     Weapon,
 )
 
-from ..utils_data import DatasetLoader
+from ..utils_data import DatasetLoader, Sampler
 
 
 class WikiEventsDatasetLoader(DatasetLoader):
@@ -492,4 +499,107 @@ class WikiEventsDatasetLoader(DatasetLoader):
                 self.elements[key]["events"] += events
                 self.elements[key]["arguments"] += arguments
                 self.elements[key]["gold"] += entities  # Is not used anyway
+
+
+class WikiEventsSampler(Sampler):
+    """
+    A data `Sampler` for the WikiEvents dataset.
+
+    Args:
+        dataset_loader (`WikiEventsDatasetLoader`):
+            The dataset loader that contains the data information.
+        task (`str`, optional):
+            The task to sample. It must be one of the following: NER, EE, EAE.
+            Defaults to `None`.
+        split (`str`, optional):
+            The split to sample. It must be one of the following: "train", "dev" or
+            "test". Depending on the split the sampling strategy differs. Defaults to
+            `"train"`.
+        parallel_instances (`Union[int, Tuple[int, int]]`, optional):
+            The number of sentences sampled in parallel. Options:
+
+                * **`int`**: The amount of elements that will be sampled in parallel.
+                * **`tuple`**: The range of elements that will be sampled in parallel.
+
+            Defaults to 1.
+        max_guidelines (`int`, optional):
+            The number of guidelines to append to the example at the same time. If `-1`
+            is given then all the guidelines are appended. Defaults to `-1`.
+        guideline_dropout (`float`, optional):
+            The probability to dropout a guideline definition for the given example. This
+            is only applied on training. Defaults to `0.0`.
+        seed (`float`, optional):
+            The seed to sample the examples. Defaults to `0`.
+        prompt_template (`str`, optional):
+            The path to the prompt template. Defaults to `"templates/prompt.txt"`.
+        ensure_positives_on_train (bool, optional):
+            Whether to ensure that the guidelines of annotated examples are not removed.
+            Defaults to `False`.
+        dataset_name (str, optional):
+            The name of the dataset. Defaults to `None`.
+        scorer (`str`, optional):
+           The scorer class import string. Defaults to `None`.
+        sample_only_gold_guidelines (`bool`, optional):
+            Whether to sample only guidelines of present annotations. Defaults to `False`.
+    """
+
+    def __init__(
+        self,
+        dataset_loader: WikiEventsDatasetLoader,
+        task: str = None,
+        split: str = "train",
+        parallel_instances: Union[int, Tuple[int, int]] = 1,
+        max_guidelines: int = -1,
+        guideline_dropout: float = 0.0,
+        seed: float = 0,
+        ensure_positives_on_train: bool = False,
+        dataset_name: str = None,
+        scorer: str = None,
+        sample_only_gold_guidelines: bool = False,
+        **kwargs,
+    ) -> None:
+        assert task in [
+            "NER",
+            "EE",
+            "EAE",
+        ], f"{task} must be either 'NER', 'EE', 'EAE'."
+
+        task_definitions, task_target, task_template = {
+            "NER": (ENTITY_DEFINITIONS, "entities", "templates/prompt.txt"),
+            "EE": (COARSE_EVENT_DEFINITIONS, "events", "templates/prompt.txt"),
+            "EAE": (EVENT_DEFINITIONS, "arguments", "templates/prompt_ace_eae.txt"),
+        }[task]
+
+        if task in ["EAE"]:
+            is_coarse_to_fine: bool = True
+            COARSE_TO_FINE = COARSE_TO_FINE_EVENTS
+            FINE_TO_COARSE = FINE_TO_COARSE_EVENTS
+        else:
+            is_coarse_to_fine = False
+            COARSE_TO_FINE = None
+            FINE_TO_COARSE = None
+
+        kwargs.pop("prompt_template")
+
+        super().__init__(
+            dataset_loader=dataset_loader,
+            task=task,
+            split=split,
+            parallel_instances=parallel_instances,
+            max_guidelines=max_guidelines,
+            guideline_dropout=guideline_dropout,
+            seed=seed,
+            prompt_template=task_template,
+            ensure_positives_on_train=ensure_positives_on_train,
+            sample_only_gold_guidelines=sample_only_gold_guidelines,
+            dataset_name=dataset_name,
+            scorer=scorer,
+            task_definitions=task_definitions,
+            task_target=task_target,
+            is_coarse_to_fine=is_coarse_to_fine,
+            coarse_to_fine=COARSE_TO_FINE,
+            fine_to_coarse=FINE_TO_COARSE,
+            definitions=GUIDELINES,
+            **kwargs,
+        )
 
