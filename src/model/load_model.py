1c02969345ff655f98f7c4ba85f938ebc43c5cc1
ikergarciaf <igarciaf896@gmail.com>
2023-06-27 13:10:42 +0200

    Fix device_map in multi_gpu environments

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -22,6 +22,38 @@ from transformers.models.auto.modeling_auto import (
 from .model_utils import get_trainable_parameters
 
 
+def get_device_map(force_auto_device_map: bool) -> str:
+    """
+    Get the device map to use for loading the model
+
+    Args:
+        force_auto_device_map (`bool`):
+            Whether to force the use of the auto device map. If set to True, the model will be split across
+            GPUs and CPU to fit the model in memory. If set to False, a full copy of the model will be loaded
+            into each GPU.
+    Returns:
+        `str`:
+            The device map to use for loading the model
+    """
+    if force_auto_device_map:
+        logging.info("Using auto device map, we will split the model across GPUs and CPU to fit the model in memory.")
+        device_map = "auto"
+    else:
+        word_size = int(os.environ.get("LOCAL_WORLD_SIZE", 1))
+        if word_size > 1:
+            logging.info(
+                f"Found DDP environment and force_auto_device_map is set to False, we will load a copy of the model "
+                f"on each GPU."
+            )
+            device_map = {"": int(os.environ.get("LOCAL_RANK", 0))}
+        else:
+            device_map = None
+
+    logging.info(f"We will load the model using the following device map: {device_map}")
+
+    return device_map
+
+
 def load_model_for_training(
     model_weights_name_or_path: str,
     quantization: Optional[int] = None,
@@ -99,23 +131,8 @@ def load_model_for_training(
             "argument (e.g 'adamw_bnb_8bit', 'lion_8bit', 'paged_adamw_8bit', ...)."
         )
 
-    if force_auto_device_map:
-        word_size = int(os.environ.get("LOCAL_WORLD_SIZE", 1))
-        if word_size > 1:
-            logging.info(
-                f"Using auto device map with DDP. The model will be split across {word_size} GPUs and CPU to fit the"
-                " model in memory."
-            )
-            device_map = {"": int(os.environ.get("LOCAL_RANK", 0))}
-        else:
-            logging.info("Using auto device map with with a single GPU or CPU. The model will be split between them.")
-            device_map = "auto"
-    else:
-        device_map = None
-
-    logging.info(f"We will load the model using the following device map: {device_map}")
-
     logging.info(f"Loading model model from {model_weights_name_or_path}")
+    device_map = get_device_map(force_auto_device_map=force_auto_device_map)
 
     MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.update(
         {
@@ -313,23 +330,8 @@ def load_model_for_inference(
         quantization in [4, 8]
     ), f"Quantization must be 4 or 8, or None for FP32/FP16 training. You passed: {quantization}"
 
-    if force_auto_device_map:
-        word_size = int(os.environ.get("LOCAL_WORLD_SIZE", 1))
-        if word_size > 1:
-            logging.info(
-                f"Using auto device map with DDP. The model will be split across {word_size} GPUs and CPU to fit the"
-                " model in memory."
-            )
-            device_map = {"": int(os.environ.get("LOCAL_RANK", 0))}
-        else:
-            logging.info("Using auto device map with with a single GPU or CPU. The model will be split between them.")
-            device_map = "auto"
-    else:
-        device_map = None
-
-    logging.info(f"We will load the model using the following device map: {device_map}")
-
     logging.info(f"Loading model from {weights_path}")
+    device_map = get_device_map(force_auto_device_map=force_auto_device_map)
 
     MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.update(
         {
