c63b4cad5d65d375113e8ce5db2c6cf30c2766fc
OSainz <osainz59@gmail.com>
2023-07-04 09:53:53 +0200

    Prepare data for huge experiments

--- bash_scripts/generate_data.sh
+++ bash_scripts/generate_data.sh
@@ -5,6 +5,23 @@ source /gscratch4/users/osainz006/CoLLIE/venv/collie/bin/activate
 CONFIG_DIR="configs/data_configs"
 OUTPUT_DIR="data/processed"
 
+# python -m src.generate_data \
+#     --configs \
+#         ${CONFIG_DIR}/ace_config.json \
+#         ${CONFIG_DIR}/bc5cdr_config.json \
+#         ${CONFIG_DIR}/conll03_config.json \
+#         ${CONFIG_DIR}/diann_config.json \
+#         ${CONFIG_DIR}/europarl_config.json \
+#         ${CONFIG_DIR}/multinerd_config.json \
+#         ${CONFIG_DIR}/ncbidisease_config.json \
+#         ${CONFIG_DIR}/ontonotes_config.json \
+#         ${CONFIG_DIR}/rams_config.json \
+#         ${CONFIG_DIR}/tacred_config.json \
+#         ${CONFIG_DIR}/wikievents_config.json \
+#         ${CONFIG_DIR}/wnut17_config.json \
+#     --output ${OUTPUT_DIR} \
+#     --overwrite_output_dir
+
 python -m src.generate_data \
     --configs \
         ${CONFIG_DIR}/ace_config.json \
@@ -12,7 +29,6 @@ python -m src.generate_data \
         ${CONFIG_DIR}/conll03_config.json \
         ${CONFIG_DIR}/diann_config.json \
         ${CONFIG_DIR}/europarl_config.json \
-        ${CONFIG_DIR}/multinerd_config.json \
         ${CONFIG_DIR}/ncbidisease_config.json \
         ${CONFIG_DIR}/ontonotes_config.json \
         ${CONFIG_DIR}/rams_config.json \
@@ -25,6 +41,24 @@ python -m src.generate_data \
 # Generate baseline data
 OUTPUT_DIR="data/baseline"
 
+# python -m src.generate_data \
+#     --configs \
+#         ${CONFIG_DIR}/ace_config.json \
+#         ${CONFIG_DIR}/bc5cdr_config.json \
+#         ${CONFIG_DIR}/conll03_config.json \
+#         ${CONFIG_DIR}/diann_config.json \
+#         ${CONFIG_DIR}/europarl_config.json \
+#         ${CONFIG_DIR}/multinerd_config.json \
+#         ${CONFIG_DIR}/ncbidisease_config.json \
+#         ${CONFIG_DIR}/ontonotes_config.json \
+#         ${CONFIG_DIR}/rams_config.json \
+#         ${CONFIG_DIR}/tacred_config.json \
+#         ${CONFIG_DIR}/wikievents_config.json \
+#         ${CONFIG_DIR}/wnut17_config.json \
+#     --output ${OUTPUT_DIR} \
+#     --overwrite_output_dir \
+#     --baseline
+
 python -m src.generate_data \
     --configs \
         ${CONFIG_DIR}/ace_config.json \
@@ -32,7 +66,6 @@ python -m src.generate_data \
         ${CONFIG_DIR}/conll03_config.json \
         ${CONFIG_DIR}/diann_config.json \
         ${CONFIG_DIR}/europarl_config.json \
-        ${CONFIG_DIR}/multinerd_config.json \
         ${CONFIG_DIR}/ncbidisease_config.json \
         ${CONFIG_DIR}/ontonotes_config.json \
         ${CONFIG_DIR}/rams_config.json \
