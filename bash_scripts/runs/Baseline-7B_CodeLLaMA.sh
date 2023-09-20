7c501fbc1d61a5d6ab6cb5c78e115cb7042e7713
Iker <igarciaf896@gmail.com>
2023-09-20 12:50:44 +0200

    Remove legacy and 3 runs files

---
+++ bash_scripts/runs/Baseline-7B_CodeLLaMA.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=Baseline-7B_CodeLLaMA
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128G
+#SBATCH --output=.slurm/Baseline-7B_CodeLLaMA.out.txt
+#SBATCH --error=.slurm/Baseline-7B_CodeLLaMA.err.txt
+
+
+source /ikerlariak/osainz006/venvs/collie/bin/activate
+
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export TOKENIZERS_PARALLELISM=true
+export TRANSFORMERS_NO_ADVISORY_WARNINGS=true
+export WANDB_ENTITY=hitz-collie
+export WANDB_PROJECT=CoLLIEv1.0
+
+echo CUDA_VISIBLE_DEVICES "${CUDA_VISIBLE_DEVICES}"
+
+# Call this script from root directory as: sbatch bash_scripts/runs/Baseline-7B_CodeLLaMA.sh
+
+for i in 2 3
+do
+CONFIGS_FOLDER="configs/model_configs/runs/$i"
+python3 -m src.run ${CONFIGS_FOLDER}/Baseline-7B_CodeLLaMA.yaml
+done
