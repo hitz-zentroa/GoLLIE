8cd37321cd7b14326f012fbc26a9e0fc2f32b3e8
Iker <igarciaf896@gmail.com>
2023-10-23 17:37:04 +0200

    Eval 128

--- bash_scripts/eval/GoLLIE-7B_CodeLLaMA_128.sh
+++ bash_scripts/eval/GoLLIE-7B_CodeLLaMA_128.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=GoLLIE-7B_CodeLLaMA_eval
+#SBATCH --job-name=GoLLIE-7B_CodeLLaMA_eval_128
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:4
 #SBATCH --mem=200G
-#SBATCH --output=.slurm/GoLLIE-7B_CodeLLaMA_eval.out.txt
-#SBATCH --error=.slurm/GoLLIE-7B_CodeLLaMA_eval.err.txt
+#SBATCH --output=.slurm/GoLLIE-7B_CodeLLaMA_eval_128.out.txt
+#SBATCH --error=.slurm/GoLLIE-7B_CodeLLaMA_eval_128.err.txt
 
 
 source /ikerlariak/osainz006/venvs/collie/bin/activate
@@ -30,4 +30,4 @@ CONFIGS_FOLDER="configs/model_configs/eval"
 
 # python3 -m src.run ${CONFIGS_FOLDER}/GoLLIE-7B_CodeLLaMA.yaml
 
-torchrun --standalone --master_port 37229 --nproc_per_node=4  src/run.py ${CONFIGS_FOLDER}/GoLLIE-7B_CodeLLaMA.yaml
+torchrun --standalone --master_port 37225 --nproc_per_node=4  src/run.py ${CONFIGS_FOLDER}/GoLLIE-7B_CodeLLaMA_128.yaml
