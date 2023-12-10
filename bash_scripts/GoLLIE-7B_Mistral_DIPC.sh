dc7e57095451a77678a068c1b1e7726cced9b4e3
Iker <igarciaf896@gmail.com>
2023-12-10 16:43:11 +0800

    8 gpus

--- bash_scripts/GoLLIE-7B_Mistral_DIPC.sh
+++ bash_scripts/GoLLIE-7B_Mistral_DIPC.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=ixa
 #SBATCH --partition=ixa
 #SBATCH --job-name=GoLLIE-7B-Mistral
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=22
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:a100:4
-#SBATCH --mem=500G
+#SBATCH --gres=gpu:a100:8
+#SBATCH --mem=900G
 #SBATCH --output=.slurm/GoLLIE-7B-Mistral.out.txt
 #SBATCH --error=.slurm/GoLLIE-7B-Mistral.err.txt
 
@@ -21,7 +21,7 @@ export TOKENIZERS_PARALLELISM=true
 export TRANSFORMERS_NO_ADVISORY_WARNINGS=true
 export WANDB_ENTITY=hitz-collie
 export WANDB_PROJECT=GoLLIEv1.0
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=16
 
 echo CUDA_VISIBLE_DEVICES "${CUDA_VISIBLE_DEVICES}"
 
@@ -32,7 +32,7 @@ echo CUDA_VISIBLE_DEVICES "${CUDA_VISIBLE_DEVICES}"
 
 # python3 -m src.run ${CONFIGS_FOLDER}/GoLLIE-7B_CodeLLaMA_BS32_R8.yaml
 export PYTHONPATH="$PYTHONPATH:$PWD"
-torchrun --standalone --master_port 37227 --nproc_per_node=4 src/run.py configs/model_configs/GoLLIE-7B_Mistral_BS128_R128.yaml
-torchrun --standalone --master_port 37227 --nproc_per_node=4 src/run.py configs/model_configs/eval/GoLLIE-7B_Mistral_BS128_R128.yaml
+torchrun --standalone --master_port 37227 --nproc_per_node=8 src/run.py configs/model_configs/GoLLIE-7B_Mistral_BS128_R128.yaml
+torchrun --standalone --master_port 37227 --nproc_per_node=8 src/run.py configs/model_configs/eval/GoLLIE-7B_Mistral_BS128_R128.yaml
 
 
